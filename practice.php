bfa724c52fed5d77e3c525fcd6c3b07d7eac93e2
SergioGonzalezB11 <sagonzalezbarajas@gmail.com>
2023-05-17 13:28:59 -0500

    actualizacion

---
+++ practice.php
@@ -0,0 +1,22 @@
+<?php
+// imprimir  en la ventana del navegador
+//  echo 'Hola mundo';
+
+// printf es parecido a echo pero solo puede imprimir una cadena de texto a la vez.
+//%s = para concatenar string
+//%d = para concatenar numero
+//$texto = "Mundo";
+//$numero = 5;
+//printf("hola %d", $numero)
+
+//declarar una variable numerica
+$edad = 25;
+
+//Declarar una variable de texto 
+$nombre = "Sergio";
+
+// Declarar una variable booleana
+$es_valido = true ;
+echo var_dump($nombre);
+
+?>  
